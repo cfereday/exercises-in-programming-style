785e0b5fd951bacb71ac7089ebfd195e9d5af91f
Charlotte Fereday <cfereday@thoughtworks.com>
2018-07-29 14:03:10 +0100

    Charlotte: given a sentence the program will return it's unique words and count of words in the sentence as a collection of maps

--- src/style/core.clj
+++ src/style/core.clj
@@ -2,5 +2,27 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(defn frequency-task [sentence]
+
+(defn count-word
+  [word sentence]
+  (count (re-seq (re-pattern word) (str/lower-case sentence))))
+
+(defn map-words-and-count
+  [unique-words sentence]
+  (map #(assoc {} :word %1 :count (count-word %1 sentence)) unique-words))
+
+(defn lower-case-words
+  [sentence]
   (str/split (str/lower-case sentence) #" "))
+
+(defn unique-words
+  [sentence]
+  (set sentence))
+
+(defn frequency-task [sentence]
+  (let [words (lower-case-words sentence)
+        unique-words (unique-words words)
+        mapped-sentence (map-words-and-count unique-words sentence)]
+    mapped-sentence))
+
+
--- test/style/core_test.clj
+++ test/style/core_test.clj
@@ -4,9 +4,27 @@
             [clojure.string :as str]))
 
 (deftest style-test
-  (testing "Given a sentence the return will be map of the individual words decapitalised & in a vector"
+  (testing "Will conform an output to lower case"
     (let [sentence "I knew you Were trouble When you Walked In"
-          result (frequency-task sentence)
+          result (lower-case-words sentence)
           expected-result ["i" "knew" "you" "were" "trouble" "when" "you" "walked" "in"]]
-      (is (= result expected-result)))
-    ))
+      (is (= result expected-result))))
+
+  (testing "Will select the unique words"
+    (let [sentence "I knew you Were trouble When you Walked In"
+          decapitalised (lower-case-words sentence)
+          result (unique-words decapitalised)
+          expected-result #{"i" "knew" "you" "were" "trouble" "when" "walked" "in"}]
+      (is (= result expected-result))))
+  (testing "Will return a map of the words and counts as keys"
+    (let [sentence "I am am you you you"
+          decapitalised (lower-case-words sentence)
+          result (unique-words decapitalised)
+          result (map-words-and-count result sentence)
+          expected-result '({:word "i" :count 1} {:word "am" :count 2} {:word "you" :count 3})]
+      (is (= (sort-by :count result) (sort-by :count expected-result)))))
+  (testing "Given a string will return a map with its unique words and count as keys"
+    (let [sentence "I am am you you you"
+          result (frequency-task sentence)
+          expected-result '({:word "i" :count 1} {:word "am" :count 2} {:word "you" :count 3})]
+      (is (= (sort-by :count result) (sort-by :count expected-result))))))
